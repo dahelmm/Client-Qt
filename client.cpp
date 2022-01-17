8a5617d49a6edbf46e3fb85ac478558f7f28ab4c
dahelmm <dahelmm@mail.ru>
2022-01-17 18:04:16 +0300

    1

--- client.cpp
+++ client.cpp
@@ -13,3 +13,58 @@ Client::~Client()
     delete ui;
 }
 
+void Client::socketConnected()
+{
+    qDebug()<<"Подключено к серверу.";
+    printMessage("Подключено к серверу");
+    QString name = ui->lE_name->text();
+    socket->write(name.toUtf8()+"подключился к чату");
+    ui->pB_connectToServer->setText("Отключиться");
+    connectToHost=true;
+}
+
+void Client::socketDisconnected()
+{
+    qDebug() << "Отключено от сервера.";
+    printMessage("Отключено от сервера");
+    ui->pB_connectToServer->setText("Подключиться");
+    connectToHost = false;
+}
+
+void Client::socketReadyRead()
+{
+    ui->tE_info->append(socket->readAll());
+}
+
+void Client::on_pB_connectToServer_clicked()
+{
+    if(!connectToHost)
+    {
+        socket = new QTcpSocket();
+
+        connect(socket,SIGNAL(connected()),this,SLOT(socketConnected()));
+        connect(socket,SIGNAL(disconnected()),this,SLOT(socketDisconnected()));
+        connect(socket,SIGNAL(readyRead()),this,SLOT(socketReadyRead()));
+
+        socket->connectToHost("LocalHost",8001);
+    }
+    else
+    {
+        QString name = ui->lE_name->text();
+        socket->write(name.toUtf8()+"уже находится в чате");
+        socket->disconnectFromHost();
+    }
+}
+
+void Client::on_pB_sendData_clicked()
+{
+    QString name= ui->lE_name->text();
+    QString message = ui->lE_sendData->text();
+    socket->write(name.toUtf8()+":"+message.toUtf8());
+    ui->lE_sendData->clear();
+}
+
+void Client::printMessage(QString message)
+{
+    ui->tE_info->append(message);
+}
--- client.h
+++ client.h
@@ -2,6 +2,8 @@
 #define CLIENT_H
 
 #include <QMainWindow>
+#include <QTcpServer>
+#include <QTcpSocket>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Client; }
@@ -15,7 +17,22 @@ public:
     Client(QWidget *parent = nullptr);
     ~Client();
 
+private slots:
+    //обработка сигналов сокета
+    void socketConnected();
+    void socketDisconnected();
+
+    void socketReadyRead();
+
+    void on_pB_connectToServer_clicked();
+
+    void on_pB_sendData_clicked();
+
 private:
     Ui::Client *ui;
+    QTcpSocket *socket; //сокет
+    bool connectToHost; //признак подключения
+
+    void printMessage(QString message);
 };
 #endif // CLIENT_H
